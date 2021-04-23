dab0b4d58ad401bb1cca33debab5b7228bfeea5e
matheus-luciano <matheus_luciano@hotmail.com>
2021-04-23 15:42:11 -0300

    third lesson

--- main.js
+++ main.js
@@ -4,7 +4,9 @@ const app = Vue.createApp({
             product: 'Socks',
             description: "A sock is a piece of clothing worn on the feet and often covering the ankle or some part of the calf. Some type of shoe or boot is typically worn over socks. In ancient times, socks were made from leather or matted animal hair. In the late 16th century, machine-knit socks were first produced. Until 1800 both hand knitting and machine knitting were used to produce socks, but after 1800, machine knitting became the predominant method.",
             image: './assets/images/socks_green.jpg',
-            url: 'https://github.com/matheus-luciano'
+            url: 'https://github.com/matheus-luciano',
+            inventory: 9,
+            onSale: true
         }
     }
 });
